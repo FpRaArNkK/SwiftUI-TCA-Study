65bb7871f545aa0a25b0678997801dbb95826617
FpRaArNkK <mspark1184@naver.com>
2024-08-08 18:50:48 +0900

    [Add] Project init

---
+++ SwiftUI-TCA-Study/SwiftUI-TCA-Study/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-TCA-Study
+//
+//  Created by 박민서 on 8/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-TCA-Study/SwiftUI-TCA-Study/SwiftUI_TCA_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TCA_StudyApp.swift
+//  SwiftUI-TCA-Study
+//
+//  Created by 박민서 on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TCA_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
